a37b36a22c73de1ff62f55e55950769225e2f628
admin <memento370@gmail.com>
2025-09-02 09:09:25 +0300

    fix patch service

--- src/main/java/com/example/absoluteweb/server/services/PatchService.java
+++ src/main/java/com/example/absoluteweb/server/services/PatchService.java
@@ -2,7 +2,9 @@ package com.example.absoluteweb.server.services;
 
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
@@ -20,14 +22,17 @@ public class PatchService {
      * Завантаження файлу як ресурсу для повернення в HTTP-відповіді.
      */
     public Resource loadAsResource(String filename) throws Exception {
-
+        try{
             Path file = rootLocation.resolve(filename).normalize();
             Resource resource = new UrlResource(file.toUri());
             if (resource.exists() && resource.isReadable()) {
                 return resource;
             } else {
-                throw new FileNotFoundException("File not found : "+filename);
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "File not found: " + filename);
             }
+        } catch (MalformedURLException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Invalid file URL: " + filename, e);
+        }
 
     }
 }
